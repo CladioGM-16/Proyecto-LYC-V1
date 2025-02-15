7ff21110338cad2b3af391418f342ad22f4610b0
Cgm <cludiogonsales@gmail.com>
2025-02-15 14:34:44 -0500

    Primer commit - Proyecto Django

---
+++ Proyecto_lyc/__init__.py
@@ -0,0 +1,13 @@
+"""
+Proyecto_lyc/
+│── Proyecto_lyc/   # Contiene la configuración del proyecto
+|   ├── __pycache__/ # Archivos de cache (ignorar)
+│   ├── __init__.py # Archivo de inicio (no hace nada)
+│   ├── settings.py  # Configuración principal
+│   ├── urls.py      # Rutas del proyecto
+│   ├── wsgi.py      # Servidor WSGI para producción
+│   ├── asgi.py      # Servidor ASGI (para FastAPI o WebSockets)
+│── manage.py      # Comando para administrar Django
+│── db.sqlite3     # Base de datos predeterminada
+"""
+print("pruebba")
---
+++ analizador/__init__.py
@@ -0,0 +1,9 @@
+"""
+analizador/
+│   ├── __init__.py # Archivo de inicio (no hace nada, solo es un modulo de python)
+│   ├── admin.py     # Configuración del panel de administración
+│   ├── apps.py      # Configuración de la aplicación
+│   ├── models.py    # Modelos de la base de datos (las estructuras de datos )
+│   ├── tests.py     # Pruebas de la aplicación (pruebas automaticas)
+│   ├── views.py     # Vistas de la aplicación (lo que se mostrara en las pagina)
+"""
